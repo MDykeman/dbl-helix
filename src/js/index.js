352469ac25e94cee17fb95312c5f8e2aa7d8abf8
MDykeman <matthewdykeman1@gmail.com>
2018-10-17 00:07:00 -0400

    Adding Quiz to the quiz tab complete with validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,80 @@
- $(window).on('load', function () {
-     $('#welcomeModal').modal('show');
- });
+//  $(window).on('load', function () {
+//      $('#welcomeModal').modal('show');
+//  });
+
+
+$(document).ready(function () {
+
+
+
+
+    $('#welcomeModal').modal('show');
+
+    $("form[name='quiz']").validate({
+        rules: {
+            fieldRevolution: {
+                required: true
+            },
+            threeActivities: {
+                required: true
+            },
+            publishDate: {
+                required: true
+            },
+            moleculeChores: {
+                required: true
+            },
+            additionalBenefit: {
+                required: true
+            }
+        },
+        messages: {
+            fieldRevolution: {
+                required: "Please select a scientific field"
+            },
+            threeActivities: {
+                required: "Please select treu or false"
+            },
+            publishDate: {
+                required: "Please select a date"
+            },
+            moleculeChores: {
+                required: "Please select a molecule"
+            },
+            additionalBenefit: {
+                required: "Please select an additional benefit"
+            },
+        },
+        errorPlacement: function (error, element) {
+            error.appendTo(element.parents(".form-group"));
+        }
+    }); //validate
+    $("form[name='quiz']").submit(function (e) {
+        let score = 0;
+        const question1 = $("input[name='fieldRevolution']:checked").val();
+        if (question1 == 'Biology') {
+            score++;
+        }
+        const question2 = $("input[name='threeActivities']:checked").val();
+        if (question2 == 'True') {
+            score++;
+        }
+        const question3 = $("input[name='publishDate']:checked").val();
+        if (question3 == '1953') {
+            score++;
+        }
+        const question4 = $("input[name='moleculeChores']:checked").val();
+        if (question4 == 'Proteins') {
+            score++;
+        }
+        const question5 = $("input[name='additionalBenefit']:checked").val();
+        if (question5 == 'Medicine') {
+            score++;
+        }
+        $("p#quizScore").text(`You got ${score} out of 5 correct`);
+
+        $('#quizModal').modal('show');
+        e.preventDefault();
+    });
+
+});
